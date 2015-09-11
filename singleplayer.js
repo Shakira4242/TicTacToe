0b3872cc26fc2d77236964cd01e51bda59e2f2bf
Akash Shekara <akashshekara1212@gmail.com>
2015-09-11 08:31:08 -0500

    Done with everything except the AI

--- singleplayer.js
+++ singleplayer.js
@@ -11,9 +11,7 @@ $(function(){
 		toggle = 1;
 	};
 
-	function toggling(){
-		toggle = toggle ? 0 : 1;
-	};
+
 
 	function newGame(){
 		endGame = false;
@@ -41,41 +39,145 @@ $(function(){
 		playerCount = 0;
 
 		$('td').on('click',function(){
-			if($(this).html() == "" && !endGame && toggle==1){
+			if($(this).html() == "" && !endGame){
 				playerCount++;
 				$(this).html(human);
-				mv = $(this).attr('id');
+				pm = $(this).attr('id');
+				playerMoves.push(pm);
 				compMove();
 			}
 			if(GameOver()){
 				endGame = true;
+				alert("Player wins");
 			} else {
 				totalCount++;
-				toggling();
 			}
 			if(totalCount==9){
 			}
+			console.log(playerMoves);
 		});
 	}
 
-	function compMove(){
-		if(playerCount == 1){
-			if((mv == "5")){
-				randomStore = random(4);
-				if(randomStore == 1){
-					$("#1").html(computer);
-				}else if(randomStore == 2){
-					$("#3").html(computer);
-				}else if(randomStore == 3){
-					$("#9").html(computer);
-				}else if(randomStore = 4){
-					$("#7").html(computer);
-				}
+	function findIn(array,nextMove){
+		for(var i=0;i<array.length;i++){
+			if(array[i].indexOf(nextMove) !== -1){
+				return true;
 			}else{
-				if(mv == "2" || mv == "")
+				return false;
 			}
 		}
-		
+	}
+
+	computerMoves = [];
+	playerMoves = [];
+	corner = ["1","3","9","7"];
+	edge = ["2","6","8","4"];
+
+	// AI (Isn't really an AI)
+	function compMove(){
+		if(!endGame){
+			if(playerCount == 1){
+				if(pm=="5"){ 
+					randomStore = random(4);
+					if(randomStore == 1){
+						$("#1").html(computer);
+						// computermoves array gets 
+						// playermoves array gets in the initialize function
+						computerMoves.push("1");
+						// computermoves deletes from array 
+						i = corner.indexOf(computerMoves[0]);
+						delete corner[i];
+						// playermoves deletes from array
+						i = corner.indexOf(pm);
+						delete corner[i];
+
+					}else if(randomStore == 2){
+
+						$("#3").html(computer);
+						computerMoves.push("3");
+						i = corner.indexOf(computerMoves[0]);
+						delete corner[i];
+						i = corner.indexOf(pm);
+						delete corner[i];
+					}else if(randomStore == 3){
+						$("#9").html(computer);
+						computerMoves.push("9");
+						i = corner.indexOf(computerMoves[0]);
+						delete corner[i];
+						i = corner.indexOf(pm);
+						delete corner[i];
+					}else if(randomStore = 4){
+						$("#7").html(computer);
+						computerMoves.push("7");
+						i = corner.indexOf(computerMoves[0]);
+						delete corner[i];
+						i = corner.indexOf(pm);
+						delete corner[i];
+					}
+				}else{
+					// if someone doesn't choose the center we pick it
+					$("#5").html(computer);
+					computerMoves.push("5");
+					// dont forget to delete the computer moves from the existing array
+					i = corner.indexOf(computerMoves[0]);
+					delete corner[i];
+					if(corner.indexOf(pm)!==-1){
+						i = corner.indexOf(pm);
+						delete corner[i];
+					}else{
+						i = edge.indexOf(pm);
+						delete edge[i];
+					}
+				}
+				console.log(computerMoves,playerMoves);
+			}else if(playerCount == 2){
+				if(
+					(playerMoves[0]=="1" && playerMoves[1]== "4") ||
+					(playerMoves[0]=="2" && playerMoves[1]== "5") ||
+					(playerMoves[0]=="3" && playerMoves[1]== "6") ||
+					(playerMoves[0]=="7" && playerMoves[1]== "4") ||
+					(playerMoves[0]=="8" && playerMoves[1]== "5") ||
+					(playerMoves[0]=="9" && playerMoves[1]== "6") ||
+					(playerMoves[0]=="1" && playerMoves[1]== "2") ||
+					(playerMoves[0]=="4" && playerMoves[1]== "5") ||
+					(playerMoves[0]=="7" && playerMoves[1]== "8") ||
+					(playerMoves[0]=="3" && playerMoves[1]== "2") ||
+					(playerMoves[0]=="6" && playerMoves[1]== "5") ||
+					(playerMoves[0]=="9" && playerMoves[1]== "8") ||
+					(playerMoves[0]=="1" && playerMoves[1]== "5") ||
+					(playerMoves[0]=="9" && playerMoves[1]== "5") ||
+					(playerMoves[0]=="3" && playerMoves[1]== "5") ||
+					(playerMoves[0]=="7" && playerMoves[1]== "5")){
+
+						if(playerMoves[0]=="1" && playerMoves[1]== "4") {$("#7").html(computer);computerMoves.push("#7");if(corner.indexOf(pm)!==-1){
+						i = corner.indexOf(pm);
+						delete corner[i];
+					}else{
+						i = edge.indexOf(pm);
+						delete edge[i];
+					}}
+						if((playerMoves[0]=="2" && playerMoves[1]== "5")||(playerMoves[0]=="5" && playerMoves[1]== "2")) {;}
+						if((playerMoves[0]=="3" && playerMoves[1]== "6")||(playerMoves[0]=="6" && playerMoves[1]== "3")) {;}
+						if((playerMoves[0]=="7" && playerMoves[1]== "4")||(playerMoves[0]=="4" && playerMoves[1]== "7")) {;}
+						if(playerMoves[0]=="8" && playerMoves[1]== "5") {;}
+						if(playerMoves[0]=="9" && playerMoves[1]== "6") {;}
+						if(playerMoves[0]=="1" && playerMoves[1]== "2") {;}
+						if(playerMoves[0]=="4" && playerMoves[1]== "5") {;}
+						if(playerMoves[0]=="7" && playerMoves[1]== "8") {;}
+						if(playerMoves[0]=="3" && playerMoves[1]== "2") {;}
+						if(playerMoves[0]=="6" && playerMoves[1]== "5") {;}
+						if(playerMoves[0]=="9" && playerMoves[1]== "8")	{;}
+						if(playerMoves[0]=="1" && playerMoves[1]== "5") {;}
+						if(playerMoves[0]=="9" && playerMoves[1]== "5") {;}
+						if(playerMoves[0]=="3" && playerMoves[1]== "5") {;}
+						if(playerMoves[0]=="7" && playerMoves[1]== "5")	{;}
+
+				}
+			}
+		}else{
+			alert("Player wins");
+		}	
+		console.log(computerMoves);
 	}
 
 	//Declaring the winner
