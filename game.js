a2ca968456e8a33c06865b80ec77712bee95e68c
Akash Shekara <akashshekara1212@gmail.com>
2015-09-10 21:30:11 -0500

    Arranging code according to functions

--- game.js
+++ game.js
@@ -1,50 +1,25 @@
 $(function(){
 
 	function initializePlayers(){
-		player1 = {piece:0,turn:0};
-		player2 = {piece:0,turn:0};
-		getStarter(player1,player2);
-		return start();
+		curPlayer = curPlayer ? 0 : 1;
+		switchPiece();
 	};
 
-	function getStarter(player1,player2){
-		var randomnumber = Math.floor(Math.random()*2);
-		if(randomnumber===0){
-			player1.turn = true;
-			player1.piece = "x";
-			player2.turn = false;
-			player2.piece = "o";
-		}else{
-			player2.turn = true;
-			player2.piece = "x";
-			player1.turn = false;
-			player1.piece = "o";
-		}
-	};
+	function getPlayer(){
+		curPlayer = Math.floor(Math.random()*2);
+		switchPiece();
+	}
 
+	function switchPiece(){
+		curPiece = curPlayer ? 'x' : 'o';
+	}
+
+	function newGame(){
+
+	}
 
 	function start(){
-		if(player1.turn == true){
-			$("td").click(function(){
-				$(this).text(player1.piece);
-				player1.turn = false;
-				if(finished()){
-					return weHaveAWinner();
-				}else{
-					return start();
-				}
-			});
-		}else if(player1.turn == false){
-			$("td").click(function(){
-				$(this).text(player2.piece);
-				player1.turn = true;
-				if(finished()){
-					return weHaveAWinner();
-				}else{
-					return start();
-				}
-			});
-		}
+		
 	};
 
 	function weHaveAWinner(){
